522523ba6e2dd63b42b6a8fbea27bea2d45af706
nowxel <nowxel@gmail.com>
2021-02-19 18:31:24 +0200

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,92 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MyApp()
   );
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+            child: Column(
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage(
+                      'images/maxime.jpg',
+                  ),
+                ),
+                Text(
+                    'Maxime',
+                  style: TextStyle(
+                      fontFamily: 'Pacifico',
+                      fontSize: 40.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'FLUTTER DEVELOPER',
+                  style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                      color: Colors.teal.shade100,
+                      fontWeight: FontWeight.bold,
+                    letterSpacing: 2.5,
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                          Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text('+38 067 703 0 594',
+                        style: TextStyle(
+                          color: Colors.teal.shade900
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.email_outlined,
+                        color: Colors.teal,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text('nowxel@gmail.com',
+                        style: TextStyle(
+                            color: Colors.teal.shade900
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            )
+        ),
+      ),
+    );
+  }
+}
+
